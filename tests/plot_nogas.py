fbee4a887894d2fc0d65dc8bc5e7ff664306033e
Tim Haines <thaines.astro@gmail.com>
2017-01-17 14:01:15 -0600

    Change snapshots to plot

--- tests/plot_nogas.py
+++ tests/plot_nogas.py
@@ -9,14 +9,14 @@ import gadget
 
 limits = (20.0, 20.0) # kpc
 
-changa_snaps = ['ChaNGa/nogas/nogas.out.000050.tipsy', 'ChaNGa/nogas/nogas.out.000300.tipsy', 'ChaNGa/nogas/nogas.out.000500.tipsy']
-gadget_snaps = ['Gadget3/nogas/nogas_000.hdf5', 'Gadget3/nogas/nogas_007.hdf5', 'Gadget3/nogas/nogas_010.hdf5']
+changa_snaps = ['ChaNGa/nogas/nogas.tipsy', 'ChaNGa/nogas/nogas.out.000050.tipsy', 'ChaNGa/nogas/nogas.out.000150.tipsy', 'ChaNGa/nogas/nogas.out.000250.tipsy']
+gadget_snaps = ['Gadget3/nogas/nogas.hdf5', 'Gadget3/nogas/nogas_000.hdf5', 'Gadget3/nogas/nogas_002.hdf5', 'Gadget3/nogas/nogas_004.hdf5']
 
 for f in changa_snaps:
-    helpers.process_changa_snapshot(f, helpers.get_output_file(f), limits)
+    helpers.process_changa_snapshot(f, f+'.pickle', limits)
 
 for f in gadget_snaps:
-    helpers.process_gadget_snapshot(f, helpers.get_output_file(f), limits)
+    helpers.process_gadget_snapshot(f, f+'.pickle', limits)
 
 snapshots = [x for p in zip(changa_snaps, gadget_snaps) for x in p]
 
@@ -41,12 +41,11 @@ cmap.set_under(cmap(vrange[0]))
 cmap.set_over(cmap(vrange[1]))
 
 for i in range(0, len(snapshots)):
-    file = '{0:s}.pickle'.format(helpers.get_output_file(snapshots[i]))
+    file = '{0:s}.pickle'.format(snapshots[i])
     disk = helpers.load_snapshot(file)
     plotting.mesh(grid[i], disk.x_grid, disk.y_grid, disk.stellar_smd, vrange, cmap=cmap, cbar_ax=grid.cbar_axes[i],
                   xlabel=r'$x/R_s$', ylabel=r'$y/R_s$', cbar_label=r'$\Sigma_*$', norm=colors.LogNorm(),
                   title=titles(i) if i < 2 else None)
-    if i%2 == 0:
-        grid[i].text(-5.0, 15.0, r'$t=' + '{0:.2f}'.format(disk.time) + r'\,\rm{Gyrs}$', fontsize=9)
+    grid[i].text(-5.0, 15.0, r'$t=' + '{0:.2f}'.format(disk.time) + r'\,\rm{Gyrs}$', fontsize=9)
 
 plotting.save_fig(fig, 'nogas.png')
